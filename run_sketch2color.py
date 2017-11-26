820c078003191dcc38459d9f15d4a207292fc537
R VIGNESH <vigneshr9619@gmail.com>
2017-11-26 09:14:23 +0530

    Added visdom visualization

--- run_sketch2color.py
+++ run_sketch2color.py
@@ -16,8 +16,10 @@ import torch.optim as optim
 from tensorboardX import SummaryWriter
 
 vis=visdom.Visdom()
-discriminator_path = '/home/vignesh/miniproject/discriminator.pt'
-generator_path = '/home/vignesh/miniproject/generator.pt'
+discriminator_path = 'discriminator.pt'
+generator_path = 'generator.pt'
+
+#rescale from [-1,1] to [0,255]
 def convert_to_rgb(one):
     one=one+1;
     three=one*127
@@ -29,8 +31,8 @@ def run():
     ones_label = torch.ones(4)
     zeros_label = torch.zeros(4)
     
-    
     discriminator=model.Discriminator_Model()
+    #Load discriminator parameters if available
     if os.path.isfile(discriminator_path):
         print "reusing discriminator model parameters"
         discriminator.load_state_dict(torch.load(discriminator_path))
@@ -39,12 +41,16 @@ def run():
         discriminator.apply(model.weights_init)
     
     generator=model.Generator_model()
-    generator.apply(model.weights_init)
+    #Load generated parameters if available
+        
     if os.path.isfile(generator_path):
         print "reusing generator model parameters"
         generator.load_state_dict(torch.load(generator_path))
     else:
         print "created generator model with fresh parameters"
+        generator.apply(model.weights_init)
+        
+    #send data to gpu
     discriminator=discriminator.cuda()
     generator=generator.cuda()
     edges_batch=edges_batch.cuda()
@@ -68,31 +74,40 @@ def run():
     for iter in range(0,max_iter):     
         print "iteration {}".format(iter)
         for batch,(edge,color) in enumerate(training_loader):
+            #Train discriminator on real data
             discriminator.zero_grad()
             edges_batch.data.resize_(edge.size()).copy_(edge)
             color_batch.data.resize_(color.size()).copy_(color)
             D_real=discriminator(edges_batch,color_batch)
-            D_x_y = D_real.data.mean()
             
+            #Train discriminator on fake(generated) data
             G_fake = generator(edges_batch)
             D_fake = discriminator(edges_batch, G_fake.detach())
-            D_x_gx = D_fake.data.mean()
             D_loss = losses.dloss(D_real,D_fake)
             D_loss.backward()
             D_solver.step()
             
+            #train generatpr
             generator.zero_grad()
             G_fake = generator(edges_batch)
-              #vis.images(G_fake.transpose())
-            #writer.add_image('Generated Images ',torch.cat([edges_batch.data.permute(0,2,3,1),G_fake.data.permute(0,2,3,1)],2)
             D_fake = discriminator(edges_batch,G_fake)
             D_x_y = D_fake.data.mean()
             G_loss=losses.gloss(G_fake,D_fake,color_batch)
             G_loss.backward()
             G_solver.step()
-            if (batch+1)%5 == 0:
-                vis.images(torch.cat([torch.cat([convert_to_rgb(edges_batch.data),convert_to_rgb(edges_batch.data),convert_to_rgb(edges_batch.data)],1),G_fake.data],3).cpu().numpy(),opts=dict(title='Images', caption='iter {}'.format(iter)),)
-                print "batch {} D loss : {} G loss: {} D_x D_gx ".format(batch,D_loss.data[0],G_loss.data[0],D_x_y,D_x_gx) 
+            
+            #print progress and visualize the images generated
+            if (batch+1)%100 == 0:
+                #Edge Images
+                vis.images(torch.cat([convert_to_rgb(edges_batch.data),convert_to_rgb(edges_batch.data),convert_to_rgb(edges_batch.data)],1).cpu().numpy()[0:4],opts=dict(title='Sketch Images', caption='iter {}'.format(iter)),)
+                #Generated Images
+                vis.images( convert_to_rgb(G_fake.data).cpu().numpy()[0:4],opts=dict(title='Generated Images', caption='iter {}'.format(iter)),)
+                #Actual colored images
+                vis.images( convert_to_rgb(color_batch.data).cpu().numpy()[0:4],opts=dict(title='Expected Images', caption='iter {}'.format(iter)),)
+              
+                print "batch {} D loss : {} G loss: {} ".format(batch,D_loss.data[0],G_loss.data[0]) 
+                
+                #Save model parameters
                 torch.save(discriminator.state_dict(), discriminator_path)
                 torch.save(generator.state_dict(), generator_path)
         
